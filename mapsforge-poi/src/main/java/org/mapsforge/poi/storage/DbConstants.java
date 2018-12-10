0584b3130bcdcf8b6fa635b46733822345209188
Linus Kramer <linus.kramer@gmail.com>
2018-12-10 13:30:58 +0100

    make data case-insensitive

--- mapsforge-poi/src/main/java/org/mapsforge/poi/storage/DbConstants.java
+++ mapsforge-poi/src/main/java/org/mapsforge/poi/storage/DbConstants.java
@@ -21,7 +21,7 @@ package org.mapsforge.poi.storage;
 public final class DbConstants {
     public static final String CREATE_CATEGORIES_STATEMENT = "CREATE TABLE poi_categories (id INTEGER, name TEXT, parent INTEGER, PRIMARY KEY (id));";
     public static final String CREATE_CATEGORY_MAP_STATEMENT = "CREATE TABLE poi_category_map (id INTEGER, category INTEGER, PRIMARY KEY (id, category)); ";
-    public static final String CREATE_DATA_STATEMENT = "CREATE TABLE poi_data (id INTEGER, data TEXT, PRIMARY KEY (id));";
+    public static final String CREATE_DATA_STATEMENT = "CREATE TABLE poi_data (id INTEGER, data TEXT collate nocase, PRIMARY KEY (id));";
     public static final String CREATE_INDEX_STATEMENT = "CREATE VIRTUAL TABLE poi_index USING rtree(id, minLat, maxLat, minLon, maxLon);";
     public static final String CREATE_METADATA_STATEMENT = "CREATE TABLE metadata (name TEXT, value TEXT);";
     public static final String CREATE_NODES_STATEMENT = "CREATE TABLE nodes (id INTEGER, lat REAL, lon REAL, PRIMARY KEY (id));";
